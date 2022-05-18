3feeb5e2f40e05263871732eaa01ce1fd3a11dfa
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 01:48:58 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-backend-iti2"
     key            = "myfile.stfile"
-    region         = "eu-west-2"
+    region         = "us-west-2"
     dynamodb_table = "statfile"
     profile        = "default"
   }
