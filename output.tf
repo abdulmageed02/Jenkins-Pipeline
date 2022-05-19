c32d21b3ee87896a7e59b30999d3b521db1798cb
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 12:32:45 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-# output "pubEC2" {
-#   value = aws_instance.pubEC2.public_ip
-# }
+output "pubEC2" {
+  value = aws_instance.public-EC2.public_ip
+}
 
 
-# output "privateEC2" {
-#   value = aws_instance.instance.private_ip
-# }
+output "privEC2" {
+  value = aws_instance.private-EC2.private_ip
+}
 
 output "key" {
   value = tls_private_key.private_key_pair.private_key_pem
