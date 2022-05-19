2e7c0092aef7016441e37c3fc927d7a71f80dd7b
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 15:58:33 +0200

    Update SG.tf

--- SG.tf
+++ SG.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "SG-2" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = [module.network.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
   egress {
