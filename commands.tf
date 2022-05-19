e10e634775f93569a47e7f0a939c57319d36f823
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 03:17:28 +0200

    Update commands.tf

--- commands.tf
+++ commands.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "scripts" {
   provisioner "local-exec" {
     command = <<EOF
-    . ./sshConfig.sh ${aws_instance.public-EC2.public_ip} ${aws_instance.private-EC2.private_ip}
+    . ./sshConf.sh ${aws_instance.public-EC2.public_ip} ${aws_instance.private-EC2.private_ip}
     EOF
   }
 }
