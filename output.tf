3bddddd59b7dc1fd4a454d45219518e0ff25789c
abdulmageed02 <hossamabdulmageed@gmail.com>
2022-05-19 20:47:37 +0200

    update jenkinsfile

--- output.tf
+++ output.tf
@@ -12,3 +12,20 @@ output "key" {
   sensitive = true
 
 }
+output "rds_add" {
+  value = aws_db_instance.rds.address
+
+}
+output "rds_port" {
+  value = aws_db_instance.rds.port
+
+}
+output "redis_add" {
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
+
+}
+
+output "redis_port" {
+  value = aws_elasticache_cluster.redis.port
+
+}
