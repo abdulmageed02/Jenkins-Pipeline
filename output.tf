e8bb5911d8ac7e1e67aea2437d45ef39b50053ff
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 02:15:33 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output "pubEC2" {
-  value = aws_instance.pubEC2.public_ip
-}
+# output "pubEC2" {
+#   value = aws_instance.pubEC2.public_ip
+# }
 
 
-output "privateEC2" {
-  value = aws_instance.instance.private_ip
-}
+# output "privateEC2" {
+#   value = aws_instance.instance.private_ip
+# }
