3f0437c5a3fed313c759d17985d53316385b3891
abdulmageed02 <76717091+abdulmageed02@users.noreply.github.com>
2022-05-19 02:50:31 +0200

    Update commands.tf

--- commands.tf
+++ commands.tf
@@ -1,5 +1,8 @@
 resource "null_resource" "scripts" {
   provisioner "local-exec" {
-    command = echo '${tls_private_key.private_key_pair.private_key_pem}' > mykey.pem
+    command = <<EOF
+    echo '${tls_private_key.private_key_pair.private_key_pem}' > mykey.pem
+    chmod 400 mykey.pem
+    EOF
   }
 }
